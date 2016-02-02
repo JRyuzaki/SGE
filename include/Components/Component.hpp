2818a9d177067c6f8b65e1893cc8c4dd1b97a983
Jovan Zivanovic <Jovan Zivanovic>
2016-02-02 18:16:19 +0100

    Initial Class Structure

---
+++ include/Components/Component.hpp
@@ -0,0 +1,22 @@
+#ifndef __COMPONENT__
+#define __COMPONENT__
+
+class Component{
+	protected:
+		GameObject* game_object;
+	public:
+		/**
+		 * This virtual function is used as an interface for the user to make his own custom Components
+		 * for own projects.
+		 **/
+		virtual void updateComponent() = 0;
+
+		void setGameObject(GameObject* game_object){
+			this->game_object = game_object;
+		}
+
+		GameObject* getGameObject(){
+			return this->game_object;
+		}
+};
+#endif
