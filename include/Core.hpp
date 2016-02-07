7d6609260ae11f4b18c7634de60cefff5dc14319
JRyuzaki <zivanovicjovanprivatemail@gmail.com>
2016-02-07 16:49:11 +0100

    Added Core Header

---
+++ include/Core.hpp
@@ -0,0 +1,23 @@
+#ifndef __SGE__CORE__
+#define __SGE__CORE__
+
+#include <Math/Vector2.hpp>
+#include <Math/Vector3.hpp>
+
+#include <Components/Component.hpp>
+#include <Components/Renderable.hpp>
+#include <Components/Transform.hpp>
+
+#include <GameObject/GameObject.hpp>
+
+#include <Physics/CollisionEngine.hpp>
+#include <Physics/PhysicsEngine.hpp>
+
+#include <Physics/Rigidbody.hpp>
+#include <Collider/Collider.hpp>
+#include <Collider/RectCollider.hpp>
+
+#include <Render/Renderer.hpp>
+#include <Render/AnimatedSprite.hpp>
+
+#endif
