ab0c9058e736c189b7802aa8f428b4e67b8832c1
JRyuzaki <zivanovicjovanprivatemail@gmail.com>
2016-02-10 13:22:10 +0100

    Added some Attributes and changed Transform

--- include/GameObject/GameObject.hpp
+++ include/GameObject/GameObject.hpp
@@ -8,38 +8,67 @@
 #include <Math/Vector3.hpp>
 
 #include <Components/Component.hpp>
-#include <Components/Transform.hpp>
+
+#include <GameObject/Transform.hpp>
 
 namespace sge{
 	class GameObject{
 		protected:
+			static int gameobject_counter;
+
 			std::string name{"gameobject"};		///<Name of the GameObject, does not need to be unique
 			std::vector<Component*> components;
 
 			Transform transform{Vector3(0, 0, 0), Vector2(1, 1)};
 
-			static int gameobject_counter;
+			bool activity{true};
+
+			/**
+			 *Add vector list for parent relations
+			 */
 
 		public:
 			GameObject();
-			GameObject(const float spawn_x, const float spawn_y, const float spawn_z);
+			GameObject(const float spawn_x, const float spawn_y, const float spawn_z);		///<Remove?, because it can be confusing with too many params
 			GameObject(const Vector3 position, const Vector2 scale);
+			GameObject(const Vector3 position, const Vector2 scale, const double rotation);
 			~GameObject();
 
 			static const int getGameObjectCounter();
 
+			void setActivity(const bool activity);
 			void setPosition(const Vector3 position);
+			void setRotation(const double rotation);
 			void setScale(const Vector2 scale);
 			void setName(const std::string name);
+			void setTransform(const Transform &other);
 
+			const bool isActive() const;
 			const Vector3 getPosition() const;
+			const double getRotation() const;
 			const Vector2 getScale() const;
 			const std::string getName() const;
+			const Transform getTransform() const;
 
 			void addComponent(Component* component);
+			void removeComponent(Component* component);		///<Not Working Needs Fixing
+			void clearComponents();
 			std::vector<Component*>* getComponents();
 
-			virtual void updateGameObject(){};	///<This method is the interface for users to implement their own update routines for the GameObject
+			/**
+			 *Get all components of one type
+			 **/
+
+			/**
+			 *This method is called once every frame and should be used to implement own routines for custom gameobjects
+			 */
+			virtual void readyGameObject(){};		///<Better Name for this function?
+			virtual void updateGameObject(){};
+
+			/**
+			 *Add virtual destroy method
+			 *Add virtual init method
+			 **/
 
 			template<typename ComponentClass>ComponentClass* getComponent(){
 				for(Component* component : *getComponents()){
