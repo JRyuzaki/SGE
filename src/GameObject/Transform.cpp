8e7ae4d596b28d41a9c99ea9c8c88cf7481078f1
JRyuzaki <zivanovicjovanprivatemail@gmail.com>
2016-02-10 13:17:56 +0100

    Transform is no longer a component

---
+++ src/GameObject/Transform.cpp
@@ -0,0 +1,42 @@
+#include <GameObject/Transform.hpp>
+
+namespace sge{
+	Transform::Transform(Vector3 position) : position(position){
+	}
+
+	Transform::Transform(Vector3 position, Vector2 scale) : position(position), scale(scale){
+	}
+
+	Transform::Transform(Vector3 position, Vector2 scale, double rotation) : position(position), scale(scale), rotation(rotation){
+	}
+
+	Transform::Transform(const Transform &other){
+		this->position = other.getPosition();
+		this->scale = other.getScale();
+		this->rotation = other.getRotation();
+	}
+
+	void Transform::setPosition(const Vector3 position){
+		this->position = position;
+	}
+
+	const Vector3 Transform::getPosition() const{
+		return this->position;
+	}
+
+	void Transform::setScale(const Vector2 scale){
+		this->scale = scale;
+	}
+
+	const Vector2 Transform::getScale() const{
+		return this->scale;
+	}
+
+	void Transform::setRotation(const double rotation){
+		this->rotation = rotation;
+	}
+
+	const double Transform::getRotation() const{
+		return this->rotation;
+	}
+}
